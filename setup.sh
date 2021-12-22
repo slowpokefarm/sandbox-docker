5e8cca5e8e427df18baf13a5112f1ab4c80c57da
Stanislav Slow <stanislav.rimsha@gmail.com>
2021-12-22 14:33:37 +0200

    Initial

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#/!bin/sh
+
+apt-get update
+
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common \
+    git \
+    mc
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+apt-get update
+apt-get install docker-ce -y
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
